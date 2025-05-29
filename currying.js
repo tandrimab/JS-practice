c6a30781620aa9ad1ccda9f511b2837c720f6517
Tandrima Bhattacharjee <bhattacharjee.tan@outlook.com>
2025-05-29 11:12:46 +0530

    Implemented infinite currying

--- currying.js
+++ currying.js
@@ -11,7 +11,21 @@ function curry(f) {
 const join = (a, b, c) => {
     return `${a}_${b}_${c}`
  }
+
+function infinite_curry(a) {
+    return function(b) {
+        if (b) {
+            return infinite_curry(a+b)
+        } else {
+            return a
+        }
+    }
+}
+
+console.log(infinite_curry(1)(2)(3)(4)())
+
  const curriedJoin = curry(join)
  console.log(curriedJoin(1, 2, 3)) // '1_2_3'
  console.log(curriedJoin(1)(2, 3)) // '1_2_3'
  console.log(curriedJoin(1, 2)(3)) // '1_2_3'
+
